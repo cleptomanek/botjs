6518ea55aac4ed42c458d691a83345eb279f156e
cleptomanek <dziwnyadres1@gmail.com>
2019-11-13 13:15:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1941,7 +1941,7 @@ return message.channel.send({ embed });
 	      {
 			  "name": "**"+config.prefix+"help ("+config.prefix+"h)",
 			  "value": "Use this command to get more info."
-		  },
+		  }
 	};
 	return message.channel.send({ embed });
 	}
